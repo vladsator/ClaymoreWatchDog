217821c7e33d6c1c13a72d465ad4faa74f47b52f
chvladik@live.ru <Vlad97chsator>
2017-04-12 12:33:11 +0300

    Create solution

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaymoreWatchDog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
